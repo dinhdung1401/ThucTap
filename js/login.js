7287cfdc5b6de7443192cd406124ff8157d01570
dinhdungjr7 <dinhdung.jr7@gmail.com>
2019-10-16 22:12:53 +0700

    bt_html

---
+++ js/login.js
@@ -0,0 +1,32 @@
+var emailex="nguyendinhdung@gmail.com"
+var passwordex="1234567890"
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate() {
+  var email = $("#inputEmail").val();
+  var password=$("#inputPassword").val();
+  if(email!="")
+  {
+    if (validateEmail(email)) {
+      // alert(email + " is valid ");
+      if(email==emailex&&password==passwordex)
+      {
+        alert("Login successfully ");
+      }
+      else{
+        alert("Please check your email or password !")
+      }
+    } 
+    else {
+      alert(email + " is not valid ");
+    }
+    return false;
+  }
+  else{
+    alert("Enter your email address,Please !")
+  }
+}
+$("#validate").on("click", validate);
